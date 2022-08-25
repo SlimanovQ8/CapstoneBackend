97d8232ae8b7bc808a332390fe29b9f87523f69b
SlimanovQ8 <slimanovq8@gmail.com>
2022-08-25 19:06:36 +0300

    test

--- tbr3at/donation/admin.py
+++ tbr3at/donation/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, Item, Charity, Annoucement
+from .models import Category, Item, Charity, Annoucement,profile
 
 admin.site.register(Category)
 admin.site.register(Item)
 admin.site.register(Charity)
 admin.site.register(Annoucement)
+admin.site.register(profile)
--- tbr3at/donation/models.py
+++ tbr3at/donation/models.py
@@ -21,11 +21,8 @@ class Item(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField()
     image = models.ImageField(upload_to='images/')
-    rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
-    phone = models.CharField(max_length=8)
     condition = models.CharField(choices=condition_choices, max_length=15)
     isReserved = models.BooleanField(default=False)
-    place = models.CharField(max_length=200)
     category_name = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
@@ -53,6 +50,15 @@ class Annoucement(models.Model):
         ("Medium", "Medium"),
         ("Low", "Low"),
     ]
+
+    condition_choices = [
+        ("New", "New"),
+        ("Like New", "Like New"),
+        ("Used", "Used"),
+        ("any","any"),
+    ]
+
+
     description = models.TextField()
     image = models.ImageField(upload_to='images/')
     priority = models.CharField(choices=priority_choices, max_length=15)
@@ -63,4 +69,18 @@ class Annoucement(models.Model):
     )
     quantity = models.PositiveIntegerField(default=0)
     duration = models.DurationField(default= timedelta(days=1), null=True)
+    category_name = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="annoucement",
+    )
+    condition = models.CharField(choices=condition_choices, max_length=15)
+
+
+class profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    phone = models.CharField(max_length=8)
+    image = models.ImageField(upload_to='images/')
+    location = models.TextField(max_length=250)
+    points = models.PositiveIntegerField(default=0)
 
